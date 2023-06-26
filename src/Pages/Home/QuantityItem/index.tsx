bb9025fa504d7911388c6af5690bb598ee88afef
Uítalo <uitalo.urss@gmail.com>
2023-06-26 20:28:43 -0300

    colocando ação nos botoes de quantidade dos cafés

--- src/Pages/Home/QuantityItem/index.tsx
+++ src/Pages/Home/QuantityItem/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { QuantityButton, QuantityContainer, QuantityInput } from "./styles";
 import { Plus, Minus } from 'phosphor-react'
 
 
 export function QuantityItem(){
+  const [amount, setAmount] = useState(0);
+
+  function decrementAmount(){
+    if(amount > 0){
+      return setAmount(amount - 1);
+    }
+  }
+
   return(
     <QuantityContainer>
-      <QuantityButton><Minus size={14} weight="fill"/></QuantityButton>
-      <QuantityInput type="number" readOnly defaultValue={1} />
-      <QuantityButton><Plus size={14} weight="fill"/></QuantityButton>
+      <QuantityButton onClick={decrementAmount}><Minus size={14} weight="fill"/></QuantityButton>
+      <QuantityInput value={amount} type="number" readOnly />
+      <QuantityButton onClick={()=> setAmount(amount + 1)}><Plus size={14} weight="fill"/></QuantityButton>
     </QuantityContainer>
   )
 }
