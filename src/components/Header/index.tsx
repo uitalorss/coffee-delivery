be2635e5662f5230cdadda98b74753bbcaca9c1a
Uítalo <uitalo.urss@gmail.com>
2023-06-26 21:22:58 -0300

    colocando geolocalização no indicador do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,35 @@ import { HeaderContainer, Info } from "./styles";
 import logoCoffee from '../../assets/logo-coffee.svg';
 import {MapPin, ShoppingCart} from 'phosphor-react'
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Location {
+  city?: string;
+  region_code?: string;
+  country?: string;
+}
 
 
 export function Header(){
+  const [currentLocation, setCurrentLocation] = useState<Location>();
+
+  useEffect(() => {
+    geoLocation()
+  })
+
+  const geoLocation = async () => {
+    const location = await axios.get('https://ipapi.co/json')
+    setCurrentLocation(location.data);
+  }  
+
   return (
     <HeaderContainer>
       <img src={logoCoffee} alt="logo da cafeteria" />
       <Info>
         <div>
           <MapPin className="pinMap" size={22} weight="fill"/>
-          <span>Teste, TS</span>
+          <span>{currentLocation?.city}, {currentLocation?.region_code}</span>
         </div>
         <NavLink to='/'>
           <ShoppingCart className="cart" size={22} weight="fill"/>
