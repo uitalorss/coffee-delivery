299d740110b972f4ab159ebb7b88303c1c187a49
Uítalo <uitalo.urss@gmail.com>
2023-08-22 21:37:37 -0300

    ajuste na página de confirmação de checkout

--- src/Pages/Sucess/index.tsx
+++ src/Pages/Sucess/index.tsx
@@ -1,40 +1,40 @@
-import { DetailsSucess, DetailsSucessItem, SuccessContent, SucessContainer, TitleSuccess } from "./styles";
-import {MapPin, Timer, CurrencyDollar} from 'phosphor-react';
+import { OrderInfo, OrderInfoItem, SuccessContent, SucessContainer, TitleSuccess } from "./styles";
+import { MapPin, Timer, CurrencyDollar } from 'phosphor-react';
 import illustrationSuccess from '../../assets/IllustrationSuccess.png'
 
-export function Success(){
-  return(
+export function Success() {
+  return (
     <SucessContainer>
-      <TitleSuccess>Uhu! Pedido confirmado</TitleSuccess>
-      <p>Agora é só aguardar que logo o café chegará até você</p>
-      <div>
-      <SuccessContent>
-        <DetailsSucess>
-          <DetailsSucessItem>
-            <span className="mapPin"><MapPin size={16} weight="fill"/></span>
-            <ul>
-              <li>Entrega em Rua João Daniel Martinelli, 102</li>
-              <li>Farrapos - Porto Alegre, RS</li>
-            </ul>
-          </DetailsSucessItem>
-          <DetailsSucessItem>
-            <span className="timer"><Timer size={16} weight="fill"/></span>
-            <ul>
-              <li>Previsão de entrega</li>
-              <li>20min - 30 min</li>
-            </ul>
-          </DetailsSucessItem>
-          <DetailsSucessItem>
-            <span className="currencyDollar"><CurrencyDollar size={16} weight="fill"/></span>
-            <ul>
-              <li>Pagamento na entrega</li>
-              <li>Cartão de crédito</li>
-            </ul>
-          </DetailsSucessItem>
-        </DetailsSucess>
-      </SuccessContent>
-      <img src={illustrationSuccess} alt="" />
+      <div className="introPageSuccess">
+        <TitleSuccess>Uhu! Pedido confirmado</TitleSuccess>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
       </div>
-    </SucessContainer>
+      <SuccessContent>
+        <OrderInfo>
+          <OrderInfoItem>
+            <span className="mapPin"><MapPin size={16} weight="fill" /></span>
+            <div>
+              <article>Entrega em Rua João Daniel Martinelli, 102</article>
+              <article>Farrapos - Porto Alegre, RS</article>
+            </div>
+          </OrderInfoItem>
+          <OrderInfoItem>
+            <span className="timer"><Timer size={16} weight="fill" /></span>
+            <div>
+              <article>Previsão de entrega</article>
+              <strong>20min - 30 min</strong>
+            </div>
+          </OrderInfoItem>
+          <OrderInfoItem>
+            <span className="currencyDollar"><CurrencyDollar size={16} weight="fill" /></span>
+            <div>
+              <article>Pagamento na entrega</article>
+              <strong>Cartão de crédito</strong>
+            </div>
+          </OrderInfoItem>
+        </OrderInfo>
+        <img src={illustrationSuccess} alt="" />
+      </SuccessContent>
+    </SucessContainer >
   )
 }
--- src/Pages/Sucess/styles.ts
+++ src/Pages/Sucess/styles.ts
@@ -3,55 +3,56 @@ import { TitleLarge } from "../../styles/global";
 
 
 export const SucessContainer = styled.div`
-
-div{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
   margin-top: 5rem;
+  .introPageSuccess{
+    margin-bottom: 2.5rem;
+  }
 `
 
 export const SuccessContent = styled.div`
   display: flex;
+  gap: 0 2rem;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem 0;
+  width: 100%;
+  max-width: 680px;
   border: 2px solid ${props => props.theme.colors.purple};
   border-radius: 6px 36px 6px 36px;
   padding: 2.5rem;
   margin-top: 2.5rem;
 `
 
-export const DetailsSucess = styled.ul`
+export const OrderInfoItem = styled.div`
+display: flex;
+gap: 0 1rem;
+span{
   display: flex;
-  flex-direction: column;
-  gap: 2rem 0;
-`
-
-export const DetailsSucessItem = styled.li`
-  list-style: none;
-  display: flex;
-  gap: .75rem;
-  span{
-    display: flex;
-    align-items: center;
-    padding: .375rem .875rem;
-    border-radius: 100px;
-        &.mapPin{
-          background: ${props => props.theme.colors.purple};
-          color: ${props => props.theme.colors.white};
-        }
-        &.timer{
-          background: ${props => props.theme.colors.yellow};
-          color: ${props => props.theme.colors.white}
-        }
-        &.currencyDollar{
-          background: ${props => props.theme.colors["yellow-dark"]};
-          color: ${props => props.theme.colors.white}
-        }
-  }
-  ul{
-    li{
-      list-style: none;
+  align-items: center;
+  padding: .375rem .875rem;
+  border-radius: 100px;
+    &.mapPin{
+      background: ${props => props.theme.colors.purple};
+      color: ${props => props.theme.colors.white};
     }
+    &.timer{
+      background: ${props => props.theme.colors.yellow};
+      color: ${props => props.theme.colors.white}
+    }
+    &.currencyDollar{
+      background: ${props => props.theme.colors["yellow-dark"]};
+      color: ${props => props.theme.colors.white}
+    }
+  }
+  div{
+    display: flex;
+    flex-direction: column;
+    gap: .25rem;
   }
 `
 
